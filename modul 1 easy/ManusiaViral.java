2d535cb6392ed6b0f48ea4f4fa47c5f3d5f0718f
salsareike40 <salsareike40@gmail.com>
2024-10-24 23:49:36 +0800

    Add files via upload

---
+++ modul 1 easy/ManusiaViral.java
@@ -0,0 +1,15 @@
+public class ManusiaViral{
+    String nama;
+    String skills;
+    int umur;
+    String hobi;
+    Orang berikutnya;
+
+    public ManusiaViral(String nama, String skills, int umur, String hobi){
+        this.nama = nama;
+        this.skills = keterampilan;
+        this.umur = umur;
+        this.hobi = hobi;
+        this.berikutnya = null;
+    }
+}
